e681abbd4568df21d5fa8479bd272292ad0c301e
Marta Fagúndez <55920614+MartaFagundez@users.noreply.github.com>
2023-05-09 18:03:07 -0300

    Update Ejercicio 05.js

--- M03 JavaScript Fundamentos/Ejercicio 05.js
+++ M03 JavaScript Fundamentos/Ejercicio 05.js
@@ -1,6 +1,15 @@
 /*⚠️ NO MODIFIQUES EL NOMBRE DE LAS DECLARACIONES ⚠️*/
 /*5️⃣ EJERCICIO 05 5️⃣*/
 
+// REPASO
+// Suma: +
+// Resta: -
+// Multiplicación: *
+// División: /
+// Módulo o resto: %
+
+
+
 function esPositivo(num) {
    // La función recibe un entero. Devuelve como resultado un string que indica si el número
    // es positivo o negativo.
